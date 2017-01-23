471102219d23824e8faf3c7b92e78e24c8203260
André Luís Bordignon <albordignon@gmail.com>
2017-01-23 13:47:06 -0200

    Usando ng-repeat para variar as fotos

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -2,6 +2,168 @@
 
 angular.module('retratosApp')
 
+        .controller('FotosController',['$scope', function($scope){
+            $scope.fotoscasamento = [
+                {
+                    "id": 0,
+                    "carousel": 0,
+                    "name": "foto0",
+                    "image": "../images/casamentos/casa03.jpg"      
+                },
+                {
+                    "id": 1,
+                    "carousel": 0,
+                    "name": "foto1",
+                    "image": "../images/casamentos/casa02.jpg"      
+                },
+                {
+                    "id": 2,
+                    "carousel": 0,
+                    "name": "foto2",
+                    "image": "../images/casamentos/casa04.jpg"      
+                }
+            ];
+            
+            
+            $scope.fotosgestante = [
+                {
+                    "id": 0,
+                    "carousel": 1,
+                    "name": "foto0",
+                    "image": "../images/gestante/gestante01.jpg"      
+                },
+                {
+                    "id": 1,
+                    "carousel": 1,
+                    "name": "foto1",
+                    "image": "../images/gestante/gestante02.jpg"      
+                },
+                {
+                    "id": 2,
+                    "carousel": 1,
+                    "name": "foto2",
+                    "image": "../images/gestante/gestante03.jpg"      
+                },
+                {
+                    "id": 3,
+                    "carousel": 1,
+                    "name": "foto3",
+                    "image": "../images/gestante/gestante04.jpg"      
+                },
+                {
+                    "id": 4,
+                    "carousel":1,
+                    "name": "foto4",
+                    "image": "../images/gestante/gestante05.jpg"      
+                },
+                {
+                    "id": 5,
+                    "carousel":1,
+                    "name": "foto5",
+                    "image": "../images/gestante/gestante06.jpg"      
+                },
+                {
+                    "id": 6,
+                    "carousel":1,
+                    "name": "foto6",
+                    "image": "../images/gestante/gestante07.jpg"      
+                }
+            ];
+            
+            $scope.fotosinfantil = [
+                {
+                    "id": 0,
+                    "carousel": 2,
+                    "name": "foto0",
+                    "image": "../images/infantil/infantil01.jpg"      
+                },
+                {
+                    "id": 1,
+                    "carousel": 2,
+                    "name": "foto1",
+                    "image": "../images/infantil/infantil02.jpg"      
+                },
+                {
+                    "id": 2,
+                    "carousel": 2,
+                    "name": "foto2",
+                    "image": "../images/infantil/infantil03.jpg"      
+                },
+                {
+                    "id": 3,
+                    "carousel": 2,
+                    "name": "foto3",
+                    "image": "../images/infantil/infantil04.jpg"      
+                },
+                {
+                    "id": 4,
+                    "carousel": 2,
+                    "name": "foto4",
+                    "image": "../images/infantil/infantil05.jpg"      
+                },
+                {
+                    "id": 5,
+                    "carousel": 2,
+                    "name": "foto5",
+                    "image": "../images/infantil/infantil06.jpg"      
+                },
+                {
+                    "id": 6,
+                    "carousel": 2,
+                    "name": "foto6",
+                    "image": "../images/infantil/infantil07.jpg"      
+                }
+            ];
+            
+            $scope.fotosfotojornalismo = [
+                {
+                    "id": 0,
+                    "carousel": 2,
+                    "name": "foto0",
+                    "image": "../images/fotojornalismo/fotojornalismo01.jpg"      
+                },
+                {
+                    "id": 1,
+                    "carousel": 2,
+                    "name": "foto1",
+                    "image": "../images/fotojornalismo/fotojornalismo02.jpg"      
+                },
+                {
+                    "id": 2,
+                    "carousel": 2,
+                    "name": "foto2",
+                    "image": "../images/fotojornalismo/fotojornalismo03.jpg"      
+                },
+                {
+                    "id": 3,
+                    "carousel": 2,
+                    "name": "foto3",
+                    "image": "../images/fotojornalismo/fotojornalismo04.jpg"      
+                },
+                {
+                    "id": 4,
+                    "carousel": 2,
+                    "name": "foto4",
+                    "image": "../images/fotojornalismo/fotojornalismo05.jpg"      
+                },
+                {
+                    "id": 5,
+                    "carousel": 2,
+                    "name": "foto5",
+                    "image": "../images/fotojornalismo/fotojornalismo06.jpg"      
+                },
+                {
+                    "id": 6,
+                    "carousel": 2,
+                    "name": "foto6",
+                    "image": "../images/fotojornalismo/fotojornalismo07.jpg"      
+                }
+            ];
+            
+        }])
+
+
+
         .controller('ContactController', ['$scope', '$http', function($scope, $http) {
             
             $scope.user = {id:0, mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
